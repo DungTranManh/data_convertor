46d95f9429eed6c9add9e9f52bffba18d20d9409
DungTranManh <manhdung5801@gmail.com>
2022-07-26 21:35:30 +0700

    no message

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
-from sys import api_version
-from unicodedata import name
 import xml.etree.ElementTree as ET
-
+import pandas as pd
 xml_file_name = 'JMnedict.xml'
 
 tree = ET.parse(xml_file_name)
 root = tree.getroot()
+
+cols = ['ent_seq','keb','reb','name_type','trans_det']
+rows = list()
+res = tuple()
 i = 0
-res = list()
 for entry in root.findall("entry"):
-    # ent_seq = entry.findall("ent_seq")
-    # k_ele = entry.findall("k_ele")
-    # if len(k_ele) != 1:
-    #     for i in range(len(k_ele)):
-    #         keb = k_ele[i].findall("keb")
-    #         res.append(len(keb))
+    # storage components
+    reb_storage = tuple()
+    keb_storage = tuple()
+    name_type_storage = tuple()
+    trans_det_storage = tuple()
+    # get ent_seq
+    ent_seq = entry.find("ent_seq").text
+    # get keb
+    k_ele = entry.findall("k_ele")
+    for i in range(len(k_ele)):
+        keb = k_ele[i].find("keb").text
+        keb_storage += (keb,)
+    # get reb
     r_ele = entry.findall("r_ele")
-    if len(r_ele) != 1:
-        for i in range(len(r_ele)):
-            reb = r_ele[i].findall("reb")
-            res.append(len(reb))
-    # name_type = entry.find("trans").find("name_type").text
-    # trans_det = entry.find("trans").findall("trans_det")
-    # # print(f"{ent_seq},{keb},{reb},{name_type},{len(trans_det)}")
-    # i += 1
-    # if i == 4:
-    #     break
-    # res.append(len(r_ele))
-
-print(max(res))
-
+    for i in range(len(r_ele)):
+        reb = r_ele[i].find("reb").text
+        reb_storage += (reb,)
+    # get name_type and trans_det
+    trans = entry.findall("trans")
+    for i in range(len(trans)):
+        name_type = trans[i].find("name_type")
+        name_type_storage += (name_type,)
+        trans_det = trans[i].findall("trans_det")
+        for tmp in range(len(trans_det)):
+            trans_det_com = trans_det[tmp].text
+            trans_det_storage += (trans_det_com,)
+    res += ((ent_seq,keb_storage,reb_storage,trans_det_storage),)
+    i += 1
+    if i == 4:
+        break
+for i in range (5):
+    print(res[i])
