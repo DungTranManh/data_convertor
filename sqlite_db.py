5dcfdd2bb317e0493f0f94e04d53ab6a6500f4c9
DungTranManh <manhdung5801@gmail.com>
2022-07-27 15:57:51 +0700

    sqlite data convertor

---
+++ sqlite_db.py
@@ -0,0 +1,17 @@
+from json import load
+import sqlite3
+import pandas as pd
+import csv
+
+conn = sqlite3.connect("data.db")
+cursor = conn.cursor()
+# cursor.execute("create table big_data (id INTEGER PRIMARY KEY AUTOINCREMENT, ent_seq INTERGER, keb text, reb text, name_type text, trans_det text)")
+
+file = open("data.csv",encoding="utf-8")
+contents = csv.reader(file)
+
+
+cursor.executemany("INSERT INTO big_data (ent_seq,keb,reb,name_type,trans_det) VALUES (?,?,?,?,?)", contents)
+
+conn.commit()
+conn.close()
